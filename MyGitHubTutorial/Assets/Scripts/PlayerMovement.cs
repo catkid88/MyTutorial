71a265dd72766d7633e5acab7b4c32a7b0e15ee9
oeonatanian <onatanianowen@gmail.com>
2023-10-31 11:39:02 -0700

    Updated scene and player contoller

--- MyGitHubTutorial/Assets/Scripts/PlayerMovement.cs
+++ MyGitHubTutorial/Assets/Scripts/PlayerMovement.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Code for playe movement Done
+    private Vector3 moveDirection;
+    private int speed = 10;
+
+    public void Jump()
+    {
+        //player can jump
+    }
+
+    public void Movement()
+    {
+        // moves the player
+    }
 }
